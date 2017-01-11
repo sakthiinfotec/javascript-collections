0dc300b0792f02a6417a298b85230da4049b5adf
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2017-01-11 14:58:17 -0700

    Random background color function added

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 
-/* Set random color of a given HTML element */
+/* Sets random color of a given HTML element */
 function setRandomColor(elementName) {
   var elementList = document.querySelectorAll(elementName);
   for(const element of elementList) { 
@@ -11,3 +11,13 @@ function setRandomColor(elementName) {
 setRandomColor("p");  // Sets random color to all the paragraph elements
 setRandomColor("h3"); // Sets random color to all the header(h3) elements
 setRandomColor("li > a"); // Sets random color to all anchor(a) elements under list(li) elements
+
+/* Sets random background color of a given HTML element */
+function setRandomBackgroundColor(elementName) {
+  var elementList = document.querySelectorAll(elementName);
+  for(const element of elementList) { 
+    element.style.backgroundColor = "#"+Math.floor(Math.random() * (255 * 255 * 255)).toString(16);
+  }
+}
+// Example
+setRandomBackgroundColor("p");  // Sets random background color to all the paragraph elements
