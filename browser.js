6658485ffe7505de90a86f1a5c5033d05497a3eb
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2017-01-11 14:40:37 -0700

    Create browser.js

---
+++ browser.js
@@ -0,0 +1,13 @@
+
+/* Set random color of a given HTML element */
+function setRandomColor(elementName) {
+  var elementList = document.querySelectorAll(elementName);
+  for(const element of elementList) { 
+    element.style.color = "#"+Math.floor(Math.random() * 255).toString(16) + Math.floor(Math.random() * 255).toString(16) + Math.floor(Math.random() * 255).toString(16);
+  }
+}
+
+// Example
+setRandomColor("p");  // Sets random color to all the paragraph elements
+setRandomColor("h3"); // Sets random color to all the header(h3) elements
+setRandomColor("li > a"); // Sets random color to all anchor(a) elements under list(li) elements
