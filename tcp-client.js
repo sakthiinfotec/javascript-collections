d2156410b32a936e086493398864dd5f9d2c03a5
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2019-09-27 13:10:16 +0530

    Create tcp-client.js

---
+++ tcp-client.js
@@ -0,0 +1,18 @@
+var net = require ('net');
+var client = new net.Socket ();
+
+client.connect (54321, '127.0.0.1', () => {
+  console.log ('Connected to Server');
+  client.write ('Hello, Server!! Hi from Client!');
+});
+
+var i = 0;
+client.on ('data', (data) => {
+  console.log ('Received from server: ' + data);
+  i++;
+  if (i == 10) client.end ();
+});
+
+client.on ('close', () => {
+  console.log ('Connection with server closed');
+});
