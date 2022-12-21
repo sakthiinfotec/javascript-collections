9633f63fd3c440845b0564991a8951820c540ab7
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2022-12-21 23:39:07 +0530

    Update tips-and-tricks.ts

--- tips-and-tricks.ts
+++ tips-and-tricks.ts
@@ -1,3 +1,4 @@
+/* Function Scope */
 // Output: 0 1 2 3
 for(let i=0; i<=3;i++) {
     setTimeout(function cb(){
@@ -5,4 +6,16 @@ for(let i=0; i<=3;i++) {
     }, i * 1000)
 }
 
+/* Variable Scope */
+// Output: 200 200 200
+var myVar = 100;
 
+function Constructor() {
+	var myVar = 200;
+  this.myVar = 200;
+  console.log(myVar)
+  console.log(this.myVar)
+}
+
+const obj = new Constructor()
+console.log(obj.myVar);
