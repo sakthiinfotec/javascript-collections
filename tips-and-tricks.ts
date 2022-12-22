02d73c36eef2384dccd709f7faca3a30399ef7ab
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2022-12-22 23:17:12 +0530

    Update tips-and-tricks.ts

--- tips-and-tricks.ts
+++ tips-and-tricks.ts
@@ -131,3 +131,16 @@ let count = 0;
   }
   console.log(count); // 0
 })();
+
+// Mystry of this - keyword
+var length = 4;
+function callback() {
+  console.log(this.length);	// 3
+}
+const object = {
+  length: 5,
+  method() {
+    arguments[0]();
+  }
+};
+object.method(callback, 1, 2);
